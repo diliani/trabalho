9b56bc4f3578e1addd78af2f2a303cd684dc2c4d
diliani <132509989+diliani@users.noreply.github.com>
2024-08-01 13:46:48 -0300

    ç

--- js/perguntas.js
+++ js/perguntas.js
@@ -1,89 +1,82 @@
 export const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "Por anos silenciosos a Tumba de Takahat permaneceu selada e escondida da civilização, seu interior,iziam, abrigava ouro infinito, assim como também os        corpos dos exploradores franceses que ousaram se aventurar em seu interior. Bom, pelo menos foi isso que o cigano de quem você comprou um mapa da localização da tal tumba disse. Bobagem, você pensou, exceto pelo tesouro, toda tumba deve abrigar algum tesouro. Você? Você é um(a) aventureiro(a) qualquer buscando fortuna fácil, apesar deestar confiante você tem um pensamento repetitivo enquanto anda pelas ruas desérticas observando seu mapa feito de papiro:",
         alternativas: [
             {
-                texto: "Isso é assustador!",
-                afirmacao: [
-                    "No início ficou com medo do que essa tecnologia pode fazer.",
-                    "Achou assustador pensar na velocidade na qual a tecnologia está avançando."
+                texto: "Tenho o equipamento necessário pra isso?",
+                afirmacao: [ ""  
                 ],
                 proxima: 1,
             },
             {
-                texto: "Isso é maravilhoso!",
-                afirmacao: [
-                    "Quis saber como usar IA no seu dia a dia.",
-                    "Pensou que IA pode ajudar em tarefas da sua vida."
+                texto: "E se, por algum acaso, os perigos forem reais?",
+                afirmacao: [ ""
                 ],
-                proxima: 2,
+                proxima: 1,
+            },
+            {
+                texto: "Talvez seja melhor conseguir um companheiro",
+                afirmacao:[ ""
+
+                ],
+                proxima: 1,
             },
         ]
     },
     {
-        enunciado: "Utilizar uma IA pode ser aterrorizante mesmo, e foi pensando nisso que uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        enunciado: "Você tem o básico, lanterna, cantil com água, corda, umas barras de chocolate, até um revólver pra caso algo se torne apimentado, etcetera e etcetera, não tem motivo de preocupação, mas uma pessoa mais nesse ''pequeno roubo'' seria de valia. O bazar, esse é o lugar dos mercenários e batedores de carteira, suas únicas opções, escolha um companheiro:",
         alternativas: [
             {
-                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-                afirmacao: [
-                    "Conseguiu utilizar a IA para buscar informações úteis.",
-                    "Percebeu que a IA pode ajudar a encontrar informações úteis na internet de forma mais rápida e direcionada.",
-                    "Percebeu que a IA consegue explicar termos complicados de forma simplificada e isso ajudou muito suas pesquisas sobre assuntos complexos."
+                texto: "Akbar, um batedor de carteira",
+                afirmacao: [ ""
                 ],
-                proxima: 3,
+                proxima: 1,
             },
             {
-                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
-                afirmacao: [
-                    "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho.",
-                    "Achou que era muito mais fácil procurar por respostas utilizando alguns meios mais tradicionais mesmo que levasse mais tempo.",
-                    "Sentiu um pouco de medo de quais dados pessoais seus a IA poderia utilizar e por isso prefere fazer suas coisas com pouca intromissão da tecnologia."
+                texto: "Malik, um ex-escravo do Sudão",
+                afirmacao: [ ""
                 ],
-                proxima: 4,
+                proxima: 1,
+            },
+            {
+                texto: "Ramés, um assassino de aluguel",
+                afirmacao: [ ""
+                ],
+                proxima: 1,
             },
         ]
     },
     {
-        enunciado: "Um chat com IA pode ser uma boa alternativa para realizar tarefas do dia a dia e foi pensando nisso que uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        enunciado: "Sim, eram as únicas opções, você decide não dar detalhes maiores para esse tipo de pessoa, com certeza traiçoeira, apenas o diz que precisa de ''duas mãos a mais'' para proteção. Vocês vão em direção à marcação do mapa, não há nada, exceto areia, norte, sul, nordeste e sudoeste, areia; você se desespera um pouco, pensando na probabilidade de ter sido enganado(a), até que seu companheiro percebe uma diferente forma no mar de areia, uma rocha, e a toca.",
         alternativas: [
             {
-                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-                afirmacao: [
-                    "Conseguiu utilizar a IA para buscar informações úteis.",
-                    "Percebeu que a IA pode ajudar a encontrar informações úteis na internet de forma mais rápida e direcionada.",
-                    "Percebeu que a IA consegue explicar termos complicados de forma simplificada e isso ajudou muito suas pesquisas sobre assuntos complexos."
+                texto: "Você olha em sua direção, curioso",
+                afirmacao: [ ""
                 ],
-                proxima: 3,
+                proxima: 1,
             },
             {
-                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
-                afirmacao: [
-                    "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho.",
-                    "Achou que era muito mais fácil procurar por respostas utilizando alguns meios mais tradicionais mesmo que levasse mais tempo.",
-                    "Sentiu um pouco de medo de quais dados pessoais seus a IA poderia utilizar e por isso prefere fazer suas coisas com pouca intromissão da tecnologia."
+                texto: "Você pergunta o que ele encontrou",
+                afirmacao: [ ""
                 ],
-                proxima: 4,
+                proxima: 1,
             },
         ]
     },
     {
-        enunciado: "Ferramentas de busca que utilizam IA podem facilitar muito nosso processo de busca por informações. Agora imagine que você precisa participar de um debate sobre o quanto a IA impacta no trabalho do futuro. Qual posicionamento tomar?",
+        enunciado: "Não houve tempo para fala, o 'farol' fora tocado, rochas gemeram e se abriram sob seus pés, revelando um abismo escuro, por qual vocês dois caíram, e caíram mais, por uma espécie de túnel escuro, gosmento e liso, o desespero/desentendimento começou assim que caíram com um baque seco no que parecia ser um chão lajotado de pedras, estranhamente lisas. Talvez seja melhor:",
         alternativas: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                afirmacao: [
-                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
-                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
+                texto:"Acender sua lanterna",
+                afirmacao: [ ""
                 ],
-                proxima: 5,
+                proxima: 1,
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: [
-                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
-                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
+                texto: "Perguntar como está seu companheiro",
+                afirmacao: [ ""
                 ],
-                proxima: 6,
+                proxima: 1,
             },
         ]
     },
