d0a1b01755aac9216facb58035cd442084d43f26
diliani <132509989+diliani@users.noreply.github.com>
2024-08-15 15:34:34 -0300

    eee

--- js/aleatorio.js
+++ js/aleatorio.js
@@ -1,5 +1,3 @@
-const nomes = ["Fernanda", "Giuliana", "Maria Eduarda", "Marcelo", "Amanda", "Gustavo", "Gabriel"];
-
 export function aleatorio (lista){
     const posicao = Math.floor(Math.random()* lista.length);
     return lista[posicao];
