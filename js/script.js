e287e554008873e4a06b5f52212fd33647ebffd0
diliani <132509989+diliani@users.noreply.github.com>
2024-08-17 01:02:30 -0300

    u2

--- js/script.js
+++ js/script.js
@@ -75,7 +75,7 @@ function jogaNovamente() {
 
 function substituiNome() {
     for (const pergunta of perguntas) {
-        pergunta.enunciado = pergunta.enunciado.replace(/você/g, nome);
+        pergunta.enunciado = pergunta.enunciado.replace(//g, nome);
     }
 }
 
