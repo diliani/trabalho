812213fc0f7ddfdbd42c45a8005cd64f1667978e
diliani <132509989+diliani@users.noreply.github.com>
2024-08-29 03:19:50 -0300

    w

--- js/script.js
+++ js/script.js
@@ -59,7 +59,7 @@ function respostaSelecionada(opcaoSelecionada) {
 }
 
 function mostraResultado() {
-    caixaPerguntas.textContent = ``;
+    caixaPerguntas.textContent = `Obrigado`;
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
     caixaResultado.classList.add("mostrar");
