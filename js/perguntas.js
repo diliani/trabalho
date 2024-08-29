981023dfd2ee6c762609369a718065035931178a
diliani <132509989+diliani@users.noreply.github.com>
2024-08-28 23:11:43 -0300

    hist

--- js/perguntas.js
+++ js/perguntas.js
@@ -1,7 +1,7 @@
 export const perguntas = [
     /* pergunta 0 */
     {
-        enunciado: "Por anos silenciosos a Tumba de Takahat permaneceu selada e escondida da civilização, seu interior, diziam, abrigava ouro infinito, assim como também os corpos dos exploradores franceses que ousaram se aventurar em seu interior. Bom, pelo menos foi isso que o cigano de quem você comprou um mapa da localização da tal tumba disse. Bobagem, você pensou, exceto pelo tesouro, toda tumba deve abrigar algum tesouro. Você? Você é um(a) aventureiro(a) qualquer buscando fortuna fácil, apesar de estar confiante você tem um pensamento repetitivo enquanto anda pelas ruas desérticas observando seu mapa feito de papiro:",
+        enunciado: "Por anos silenciosos a Tumba de Takahat permaneceu selada e escondida da civilização, seu interior, diziam, abrigava ouro infinito, assim como também os corpos dos exploradores franceses que ousaram se aventurar em seu interior. Bom, pelo menos foi isso que o cigano de quem você comprou um mapa da localização da tal tumba disse. Bobagem, você pensou, exceto pelo tesouro, toda tumba deve abrigar algum tesouro. Você? Você é um aventureiro qualquer buscando fortuna fácil, apesar de estar confiante você tem um pensamento repetitivo enquanto anda pelas ruas desérticas observando seu mapa feito de papiro:",
         alternativas: [
             {
                 texto: "Tenho o equipamento necessário pra isso?",
@@ -50,25 +50,139 @@ export const perguntas = [
     },
     /* pergunta 2 */
     {
-        enunciado: "O cigano era um sujeito muito estranho, tinha uma voz distinta, que dava um aspecto de velhice, isso não falando da sua aparência, nada higiênica; um olho branco que dava medo. ''",
+        enunciado: "O cigano era um sujeito muito estranho, tinha uma voz distinta, que dava um aspecto de velhice, isso não falando da sua aparência, nada higiênica; um olho branco que dava medo. ''A Gruta Imunda é um lugar real, seu interior era inteiramente dourado, com milhares de toneladas e toneladas de ouro, mas aqueles franceses sarnentos tiveram que desflorar o santuário, sujá-lo com seu sangue. Se tu vai a este ninho de sujeira e poder, leva sorte e coragem contigo, leva este mapa e procura o Inseto...''",
         alternativas: [
             {
-                texto: "Você olha em sua direção, curioso",
+                texto: "Você confere seu equipamento",
+                afirmacao: [ ""
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "Tenta arranjar um companheiro",
+                afirmacao: [ ""
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Examina seu mapa",
+                afirmacao: [ ""
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    /* pergunta 3 */
+    {
+        enunciado: "Uma pessoa mais nesse ''pequeno roubo'' seria de valia. O bazar, esse é o lugar dos mercenários e batedores de carteira, suas únicas opções, escolha um companheiro:",
+        alternativas: [
+            {
+                texto: "Akbar, um batedor de carteira",
+                afirmacao: [ ""
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Malik, um ex-escravo do Sudão",
+                afirmacao: [ ""
+                ],
+                proxima: 6,
+            },
+            {
+                texto: "Ramés, um assassino de aluguel",
                 afirmacao: [ ""
                 ],
                 proxima: 7,
             },
+        ]
+    },
+    /* pergunta 4 */
+    {
+        enunciado: "No seu mapa, em vermelho carmesim, havia um X marcado, alguns metros de distância da Cidade Desértica, o que o confundiu, pois como tanta riqueza (ou desgraça) estava tão próxima da civilização ''abandonada'? Estranho, mas você deixou isso de lado, pensando qual rota seguiria:",
+        alternativas: [
             {
-                texto: "Você pergunta o que ele encontrou",
+                texto: "Bazar",
                 afirmacao: [ ""
                 ],
                 proxima: 8,
+            },
+            {
+                texto: "Fora do muro",
+                afirmacao: [ ""
+                ],
+                proxima: 9,
+            },
+            {
+                texto: "Centro da cidade",
+                afirmacao: [ ""
+                ],
+                proxima: 10,
+            },
+        ]
+    },
+    /* pergunta 5 */
+    {
+        enunciado: "A vida nas ruas pobres e empoeiradas da cidade no deserto nunca foi fácil para Akbar, que desde criança viveu de roubos e invasões, apesar de quase ter-lhe custado uma mão (mas dois dedos sim) essa é sua única opção. Ele falava bastante e parecia amigável.                 Sim, eram as únicas opções. Você acredita que será fácil lidar com Akbar mas que, por ser um ladrão, você tem uma desvantagem. Apesar da personalidade estranhamente cativante você decide não dar detalhes maiores para esse tipo de pessoa, com certeza traiçoeira, apenas o diz que precisa de proteção e, acima de tudo, direções de uma pessoa que conhece a cidade de olhos fechados.",
+        alternativas: [
+            {
+                texto: "Você observa seu mapa",
+                afirmacao: [ ""
+                ],
+                proxima: 11,
+            },
+        ]
+    },
+    /* pergunta 6 */
+    {
+        enunciado: "Malik era um homem negro retinto, 2 metros e 10, extremamente forte, não fosse sua pureza, ele seria um grande problema. Mesmo carregando marcas de chicotes nas costas e ódio devido a acontecimentos passados, Malik era uma boa pessoa, que demonstrava coragem e bondade quando falava, o que era raro. Sim, eram as únicas opções. Malik definitivamente não era como os outros mercenários, o que incomodava é que ele mal falava e parecia nunca expressar uma opinião sobre o que você tinha requerido: proteção e ajuda para roubar algo. De qualquer forma você confiava nele e sentia uma certa pena e peso no coração.",
+        alternativas: [
+            {
+                texto: "Você observa seu mapa",
+                afirmacao: [ ""
+                ],
+                proxima: 11,
+            },
+        ]
+    },
+    /* pergunta 7 */
+    {
+        enunciado: "Era um sujeito chamativo, magro e bronzeado, usava turbante branco, tinha roupas finas com pedaços de seda vermelha, brincos dourados; pouco se sabia do sujeito, mas não era segredo que Ramés fazia parte de alguma guilda de assassinos nômades habilidosos com armas brancas. Sim, eram as únicas opções. Você mal acredita que pode conseguir serviços de alguém de tão alto calibre e periculosidade no bazar. Você definitivamente não deveria dar detalhes maiores para esse tipo de pessoa, membro de guildas de assassinos com habilidades para matar em segundos, mas você provavelmente necessita de proteção. De qualquer forma você está apreensivo dos talvez interesses que Ramés pode ter quando chegarem à localização X, quando seu serviço estiver terminado.",
+        alternativas: [
+            {
+                texto: "Você observa seu mapa",
+                afirmacao: [ ""
+                ],
+                proxima: 11,
+            },
+        ]
+    },
+    /* pergunta 8 */
+    {
+        enunciado: "O bazar da Cidade Arenosa era mais como uma atração num circo de loucuras, um ecossistema que nunca é interrompido, as tendas de cores chamativas, as especiarias, um oásis de pessoas e ladrões (em poucos minutos você presenciou 4 roubos). Caminhando, você distraidamente esbarra numa caixa de maçãs, atraindo olhos e um ladrão que você consegue ver passando 3 para debaixo do seu turbante laranja, por algum motivo ele aponta para você falando algo num dialeto árabe para o ambulante, basicamente acusando você do roubo dele, julgando o fato de ter uma cimitarra sacada vindo em sua direção, você já estava preparado para correr, mas o impulso e calor do momento te fizeram sacar a pistola, que dispara dois tiros no torso do ambulante, atraindo todos os olhos do bazar, inclusive da Guarda, sem escolha você corre por todo o Deserto DeGente. Correr na estrada de poeira e areia batida com um sol de sabe-se lá quantas dezenas de graus não foi uma boa experiência.",
+        alternativas: [
+            {
+                texto: "O Deserto",
+                afirmacao: [ ""
+                ],
+                proxima: 12,
+            },
+        ]
+    },
+    /* pergunta 9 */
+    {
+        enunciado: "Por algum motivo você pensa ser mais seguro ir até o X por fora do muro da cidade. Você vai em direção ao esquisito Portão Oeste, os guardas nem parecem fazer seu trabalho, o que talvez seja bom pra você, porque isso significa 0 perguntas. Você caminha bem rente à parede, como se estivesse numa prancha com medo de cair no mar de areia ao lado. As casas deste lado oeste da cidade formam uma favela árabe, casinhas compartilhadas vertical e horizontalmente, uma vista agoniante pra ser sincero. Você estava tão distraído que nem percebeu uma Naja 5 pés à frente, era uma cuspideira, _______. Apesar da viagem quase acabar cedo demais, vocês continuam.",
+        alternativas: [
+            {
+                texto: "O Deserto",
+                afirmacao: [ ""
+                ],
+                proxima: 12,
             },
         ]
     },
     /* pergunta 2 */
     {
-        enunciado: "Sim, eram as únicas opções, você decide não dar detalhes maiores para esse tipo de pessoa, com certeza traiçoeira, apenas o diz que precisa de ''duas mãos a mais'' para proteção. Vocês vão em direção à marcação do mapa, não há nada, exceto areia, norte, sul, nordeste e sudoeste, areia; você se desespera um pouco, pensando na probabilidade de ter sido enganado(a), até que seu companheiro percebe uma diferente forma no mar de areia, uma rocha, e a toca.",
+        enunciado: "Sim, eram as únicas opções, você decide não dar detalhes maiores para esse tipo de pessoa, com certeza traiçoeira, apenas o diz que precisa de ''duas mãos a mais'' para proteção. Vocês vão em direção à marcação do mapa, não há nada, exceto areia, norte, sul, nordeste e sudoeste, areia; você se desespera um pouco, pensando na probabilidade de ter sido enganado, até que seu companheiro percebe uma diferente forma no mar de areia, uma rocha, e a toca.",
         alternativas: [
             {
                 texto: "Você olha em sua direção, curioso",
